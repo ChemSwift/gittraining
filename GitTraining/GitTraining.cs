a8331df5a48e9c3dfab5e7088d7716bb58694c10
ChemSwift <chem.swift@renweb.com>
2014-02-03 23:53:25 -0600

    Fixed number bug

--- GitTraining/GitTraining.cs
+++ GitTraining/GitTraining.cs
@@ -10,13 +10,13 @@ namespace GitTraining
            Console.WriteLine("Welcome to Git!");
            
            Console.WriteLine("Enter a number: ");
-           var num12 = Convert.ToInt32(Console.ReadLine());
+           var num1 = Convert.ToInt32(Console.ReadLine());
            
            Console.WriteLine("Enter a second number: ");
            var num2 = Convert.ToInt32(Console.ReadLine());
 
            int sum = num1 + num2;
-           Console.WriteLine("The sum of {0} and {1} is {2}", num12, num2, sum);
+           Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, sum);
            Console.ReadLine();
         }
     }
