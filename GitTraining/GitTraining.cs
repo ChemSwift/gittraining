bac02b72c4ff2f65c4124f03ba2e5cbdac2f16be
ChemSwift <chem.swift@renweb.com>
2014-02-03 23:57:44 -0600

    Changes sum to var

--- GitTraining/GitTraining.cs
+++ GitTraining/GitTraining.cs
@@ -15,7 +15,7 @@ namespace GitTraining
            Console.WriteLine("Enter a second number: ");
            var numTwo = Convert.ToInt32(Console.ReadLine());
 
-           int sum = num1 + numTwo;
+           var sum = num1 + numTwo;
            Console.WriteLine("The sum of {0} and {1} is {2}", num1, numTwo, sum);
            Console.ReadLine();
         }
