516c6e7e89eeda02e2e6079664a3b48512fbe78a
ChemSwift <chem.swift@renweb.com>
2014-02-03 19:51:32 -0600

    Added simple calculation

--- GitTraining/GitTraining.cs
+++ GitTraining/GitTraining.cs
@@ -8,7 +8,15 @@ namespace GitTraining
         {
            // Git Training
            Console.WriteLine("Welcome to Git!");
-           Console.WriteLine("Git is fun and easy.");
+           
+           Console.WriteLine("Enter a number: ");
+           var num1 = Convert.ToInt32(Console.ReadLine());
+           
+           Console.WriteLine("Enter a second number: ");
+           var num2 = Convert.ToInt32(Console.ReadLine());
+
+           var sum = num1 + num2;
+           Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, sum);
            Console.ReadLine();
         }
     }
